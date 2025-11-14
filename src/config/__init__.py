eceb9f6239e082574b78fe782308701ff0edd85a
hiirrs <hiirrs>
2025-11-14 08:40:05 +0700

    feat: __init__.py

---
+++ src/config/__init__.py
@@ -0,0 +1,15 @@
+from .config import (
+    ALERTS_OUTPUT_DIR,
+    ALERTS_INSERTED_FILENAME,
+    ALERTS_NOT_INSERTED_FILENAME,
+)
+
+
+from .config import Settings  
+
+__all__ = [
+    "ALERTS_OUTPUT_DIR",
+    "ALERTS_INSERTED_FILENAME",
+    "ALERTS_NOT_INSERTED_FILENAME",
+    "Settings",  # optional, if you want
+]
