880d81ffb8a296d55eb78a1397c5cd582be2bb68
hiirrs <hiirrs>
2025-11-14 11:13:09 +0700

    fix: importing on config

--- src/config/__init__.py
+++ src/config/__init__.py
@@ -4,12 +4,8 @@ from .config import (
     ALERTS_NOT_INSERTED_FILENAME,
 )
 
-
-from .config import Settings  
-
 __all__ = [
     "ALERTS_OUTPUT_DIR",
     "ALERTS_INSERTED_FILENAME",
     "ALERTS_NOT_INSERTED_FILENAME",
-    "Settings",  # optional, if you want
 ]
